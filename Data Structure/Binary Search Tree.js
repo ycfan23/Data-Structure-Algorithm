cd1d4bd9a93b9c58be865f7214ca5da6ab0549ef
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-02-25 19:06:46 -0600

    BST created and added insert method

--- Data Structure/Binary Search Tree.js
+++ Data Structure/Binary Search Tree.js
@@ -5,3 +5,36 @@ class Node {
     this.right = null;
   }
 }
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value)
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while(true) {
+        if (current.value > value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this
+          } else {
+            current = current.left
+          }
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            return this
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
