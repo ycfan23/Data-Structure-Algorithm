0d5393740e34078b4d367bceb013b44d6e37a122
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-02-25 21:56:19 -0600

    BFS implemented

--- Data Structure/Tree/BST/Binary Search Tree.js
+++ Data Structure/Tree/BST/Binary Search Tree.js
@@ -56,6 +56,23 @@ class BST {
     if (!found) return undefined;
     return current
   }
+
+  //implement Breadth First Search (BFS)
+  BFS() {
+    let data = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node)
+
+    while(queue.length) {
+      node = queue.shift();
+      data.push(node);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
 
 let tree = new BST();
@@ -63,4 +80,6 @@ tree.insert(10)
 tree.insert(5)
 tree.insert(20)
 tree.insert(15)
-console.log(tree.find(11))
+//console.log(tree.find(11))
+
+console.log(tree.BFS());
