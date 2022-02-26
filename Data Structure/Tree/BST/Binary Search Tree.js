57bc8c9bc7d22c83a71a57011298e6f76f30fc7d
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-02-26 10:47:37 -0600

    DFS InOrder implemented

--- Data Structure/Tree/BST/Binary Search Tree.js
+++ Data Structure/Tree/BST/Binary Search Tree.js
@@ -104,6 +104,20 @@ class BST {
     traverse(current)
     return data;
   }
+
+  DFSInOrder() {
+    let data = [];
+    let current = this.root;
+
+    function traverse(node) {
+      if (node.left) traverse(node.left)
+      data.push(node)
+      if(node.right) traverse(node.right)
+    }
+
+    traverse(current)
+    return data;
+  }
 }
 
 let tree = new BST();
