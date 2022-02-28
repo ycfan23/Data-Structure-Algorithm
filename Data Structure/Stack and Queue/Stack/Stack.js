f42d6fb03cf9b945fb0eb105c83be11f283575e2
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-02-28 11:39:45 -0600

    push and pop implemented

--- Data Structure/Stack and Queue/Stack/Stack.js
+++ Data Structure/Stack and Queue/Stack/Stack.js
@@ -7,8 +7,26 @@ class Node {
 
 class Stack {
   constructor() {
-    this.first = null;
-    this.last = null;
+    this.top = null;
     this.size = 0;
+  }
+
+  push(value) {
+    let newNode = new Node (value);
+    if (this.size === 0) {
+      this.top = newNode;
+    } else {
+      let temp = this.top;
+      this.top = newNode;
+      this.top.next = temp;
+    }
+    return this.size++;
+  }
+
+  pop() {
+    if (!this.top) return undefined;
+    let data = this.top.value;
+    this.top = this.top.next;
+    return data;
   }
 }
