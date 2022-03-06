72fcb8bb988b9ff2034d79a5169e4c96ff32d2b9
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-03-06 13:27:53 -0600

    pop method

--- Data Structure/SinglyLinkedList/SinglyLinkedList.js
+++ Data Structure/SinglyLinkedList/SinglyLinkedList.js
@@ -23,4 +23,19 @@ class SinglyLinkedList {
     }
     this.length += 1;
   }
+
+  pop() {
+    if(!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while(current.next) {
+      newTail = current;
+      current = current.next
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length -= 1;
+  }
 }
