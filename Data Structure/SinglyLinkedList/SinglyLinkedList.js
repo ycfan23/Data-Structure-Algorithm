4d2610893b216f469d4728923ccf1f97641183a7
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-03-06 16:52:39 -0600

    shift and unshift implemented

--- Data Structure/SinglyLinkedList/SinglyLinkedList.js
+++ Data Structure/SinglyLinkedList/SinglyLinkedList.js
@@ -38,4 +38,24 @@ class SinglyLinkedList {
     this.tail.next = null;
     this.length -= 1;
   }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.next = newNode;
+      newNode.next = this.head;
+    }
+    this.size += 1;
+  }
+
+  unshift() {
+    if(!this.head) return undefined;
+
+    let current = this.head;
+    this.head = current.next;
+    this.size -= 1;
+  }
 }
