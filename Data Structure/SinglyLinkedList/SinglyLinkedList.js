e8c202f0cd7017b477c4d4996beb5c866d72c813
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-03-06 13:08:11 -0600

    Singly Linked List and push method implemented

---
+++ Data Structure/SinglyLinkedList/SinglyLinkedList.js
@@ -0,0 +1,26 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+  }
+}
