f5f8ec9f9c77d94063a7dcdaf29b79cba62c0095
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-03-08 17:43:08 -0600

    reverse method added

--- Data Structure/SinglyLinkedList/SinglyLinkedList.js
+++ Data Structure/SinglyLinkedList/SinglyLinkedList.js
@@ -105,6 +105,24 @@ class SinglyLinkedList {
     this.length -= 1;
 
     return removed;
+  }
 
+  reverse() {
+    let current = this.head;
+    this.head = this.tail;
+    this.tail = current;
+
+    let prev = null;
+    let next = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = current.next;
+      current.next = prev
+
+      prev = current
+      current = next
+    }
+
+    return this;
   }
 }
